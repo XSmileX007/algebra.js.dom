9fab7013334e2492634ead6476cab70b23abe06c
Ivan Sakoman <ivan.sakoman@predavaci.algebra.hr>
2024-05-06 18:27:49 +0200

    added

--- dom.js
+++ dom.js
@@ -9,6 +9,7 @@ console.log("Window location: " + winLocation);
 
 var x = window.confirm("Ovime prihvaćate našu politiku o kolačićima");
 console.log("response = " + x);
+//debugger;
 if (x == true) {
   console.log("Korisnik odgovorio OK");
 
@@ -17,3 +18,10 @@ if (x == true) {
   console.log("Korisnik odgovorio NOK");
   window.location = "onama.html";
 }
+//console.log("end page 1");
+//window.alert("test alert");
+console.log("end page");
+
+window.setTimeout(function () {
+  console.log("ispis sa delay od 2 sec");
+}, 2000);
